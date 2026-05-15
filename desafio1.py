4cbb06537abb83505a91a110e6a9c37c8f6fa602
Guilherme Motta <gmotta1206@gmail.com>
2026-05-15 19:42:05 +0000

    faz a logica e as conversões dos valores

--- desafio1.py
+++ desafio1.py
@@ -1,6 +1,6 @@
 programa = True
-while programa == True:
-    try:
+valor_total = 0
+try:
         print("Digite o orçamento (Use . para casas decimais): ")
         orcamento = float(input())
         print("Digite o destino (cidade/país): ")
@@ -10,12 +10,17 @@ while programa == True:
         print("Digite o custo da hospedagem (em Euros): ")
         custo_hospedagem = float(input())
         print("Digite a duração da viagem (em dias): ")
-        custo_hospedagem = int(input())
+        duracao = int(input())
 
-    
-    except ValueError :
+        custo_hospedagem = custo_hospedagem * 6.10  
+        custo_hospedagem = custo_hospedagem * duracao
+        valor_total = custo_hospedagem + custo_passagem
+        
+        print(f"O valor total da hospedagem é {custo_hospedagem} reais")
+        print(f"O valor total da viagem é {valor_total} reais")
+
+except ValueError :
         print("Você enviou os dados errados")
         print("Deleting system32...💀")
-        programa = False
 
 
