8e05ceac988cd7ed3532871ca548039fb3720df0
Guilherme Motta <gmotta1206@gmail.com>
2026-05-15 20:09:50 +0000

    exibe os resultados

--- desafio1.py
+++ desafio1.py
@@ -1,4 +1,4 @@
-programa = True
+viavel = True
 valor_total = 0
 try:
         print("Digite o orçamento (Use . para casas decimais): ")
@@ -15,9 +15,17 @@ try:
         custo_hospedagem = custo_hospedagem * 6.10  
         custo_hospedagem = custo_hospedagem * duracao
         valor_total = custo_hospedagem + custo_passagem
-        
+                
         print(f"O valor total da hospedagem é {custo_hospedagem} reais")
         print(f"O valor total da viagem é {valor_total} reais")
+
+        if orcamento >= valor_total and duracao > 0:
+                print("A viagem tem o orçamento possível")
+                print(f"Sobram {orcamento - valor_total} reais")
+        else:
+                print("A viagem não tem orçamento possível")
+                print(f"Faltam {valor_total - orcamento} reais")
+
 
 except ValueError :
         print("Você enviou os dados errados")
