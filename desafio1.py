a2a5fee26f9648a7441f9f69440c61afa344bada
Guilherme Motta <gmotta1206@gmail.com>
2026-05-16 12:53:37 +0000

    Valida numeros negativos e adição de comentários

--- desafio1.py
+++ desafio1.py
@@ -12,23 +12,34 @@ try:
         print("Digite a duração da viagem (em dias): ")
         duracao = int(input())
 
+        #validação
+        if (orcamento < 0 or custo_passagem < 0 or custo_hospedagem < 0 or duracao < 0 ):
+                print("Erro: valores negativos não são permitidos.")
+                exit()
+
+        #Calculo
         custo_hospedagem = custo_hospedagem * 6.10  
         custo_hospedagem = custo_hospedagem * duracao
         valor_total = custo_hospedagem + custo_passagem
                 
+        #Mensagens finais
+        print(f"O destino escolhido é {destino}")
         print(f"O valor total da hospedagem é {custo_hospedagem} reais")
         print(f"O valor total da viagem é {valor_total} reais")
 
         if orcamento >= valor_total and duracao > 0:
                 print("A viagem tem o orçamento possível")
+                print(f"Viavel = {viavel}")
                 print(f"Sobram {orcamento - valor_total} reais")
         else:
+                viavel = False
                 print("A viagem não tem orçamento possível")
+                print(f"Viavel = {viavel}")
                 print(f"Faltam {valor_total - orcamento} reais")
 
 
 except ValueError :
-        print("Você enviou os dados errados")
+        print("Erro! Você digitou dados invalidos")
         print("Deleting system32...💀")
 
 
